fab9c33e8d351267b0a8f053973459da81f26161
TSephanya Brun <tsbrun@icloud.com>
2020-12-24 16:43:30 -0500

    commit tree dc6a2812f7018ebbde40954c56c20d087186d668

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#main', as: 'root'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/welcome', to: 'sessions#index'
+
+  get '/secret-page', to: 'secrets#show'
 end
